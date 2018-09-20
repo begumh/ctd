b6002e770794dbe8b886f5082795e70576e304b0
Haleema Begum <haleema_ali13@yahoo.com>
2018-09-20 23:24:07 +0000

    initial commit

---
+++ week_one/reading_assignment/ruby_basics.rb
@@ -0,0 +1,27 @@
+# answer each of the following to the best of your ability
+# there is always more than one way to code a response 😄
+# please use your knowledge of variable assignments in each response
+# when you are done, share your code with your instructor, push it to Github, or submit a pull request
+
+
+# output an integer
+puts 1+2
+puts 5+7
+
+# output the number of seconds in a week
+puts 7*24*60*60
+puts 7*24*3600
+
+# output a float
+puts 2.0+3.9
+puts 8.9+3.8
+
+# output a string
+puts "My name is haleema"
+puts "welcome to class"
+
+# output concatination of strings
+var1= 5
+var2= '9'
+puts var1.to_s + var2
+puts var1 + var2.to_i
